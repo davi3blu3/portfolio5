10331840fdee23a30a4679944bd90c2c7f3bbdb9
Dave Harned <dharned@gmail.com>
2017-02-21 21:19:44 -0600

    add jquery plugins

---
+++ public/scripts/mytype.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    
+    $(function() {
+        $('.typed-header').typed({
+            strings: ['Web Development.',
+                        'Process Improvement.',
+                        'Creative Problem Solving.'
+            ],
+            typeSpeed: 120,
+            startDelay: 200,
+            backDelay: 800,
+
+        });
+    })
+
+});
