b41f49e8d0787829d5f4f112601f16d723f6b257
Dave Harned <dharned@gmail.com>
2017-02-21 20:09:19 -0600

    install normalize and connect css files

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
-
 app.use(express.static(__dirname + '/public'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
 var server = app.listen('3000', function(req, res) {
     var port = server.address().port;
     console.log('Express server now listening on', port);
